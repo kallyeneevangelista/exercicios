af12f590c3cfadd69d34af8887d836a84b21a71a
Kallyene Evangelista <kallyeneevangelistaadv@gmail.com>
2022-04-07 13:57:46 -0300

    Atividades do bloco 4

---
+++ 1-fundamentos-do-desenvolvimento-web/3-block-introducao-a-html-e-css/dia4-html-semantico/script.js
@@ -0,0 +1,50 @@
+/* let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientInfo);
+ */
+
+
+/* const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = base + base + height + height;
+
+console.log(area);
+console.log(perimeter);
+ */
+
+
+/* 
+const nota = 1;
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovado(a)!")
+} else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera")
+} else {
+    console.log("Você foi reprovado(a)")
+} */
+
+
+const currentHour = 13;
+let message;
+
+if (currentHour >= 22) {
+    message = "Não deveríamos comer nada, é hora de dormir"
+} else if (currentHour >=18 && currentHour < 22) {
+    message = "Rango da noite, vamos jantar :D"
+} else if (currentHour >=14 && currentHour < 18) {
+    message = "Vamos fazer um bolo pro café da tarde?"
+} else if (currentHour >= 11 && currentHour < 14) {
+    message = "Hora do Almoço!!!"
+} else if (currentHour >= 4 && currentHour < 11) {
+    message = "Hum, cheiro de café recém passado"
+}
+
+console.log(message)
