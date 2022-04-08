c3e612c6be0fb2985d80b5a6df3d14ab69ed3bd9
Kallyene Evangelista <kallyeneevangelistaadv@gmail.com>
2022-04-08 18:55:44 -0300

    Atividades do dia 4.2

---
+++ 1-fundamentos-do-desenvolvimento-web/4-block-introducao-a-javascript-e-logica-de-programacao/dia2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,36 @@
+/* let tarefas = ["Assistir aulas da trybe", "Completar o projeto", "Estudar CSS", "Pular corda", "Preparar roteiro para happy hour", "Mandar violão para o conserto"];
+tarefas.unshift("Tirar a música Unbreak My Heart no violão");
+tarefas.push("Descansar");
+
+console.log(tarefas);
+
+tarefas.pop();
+
+console.log(tarefas);
+
+tarefas.shift();
+
+console.log(tarefas);
+
+ */
+
+
+//Exercício 1:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+
+//Exercício 2:
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu2.indexOf("Portfólio");
+
+console.log(indexOfPortfolio);
+
+
+//Exercício 3:
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu3.push("Contato")
+
+console.log(menu3);
