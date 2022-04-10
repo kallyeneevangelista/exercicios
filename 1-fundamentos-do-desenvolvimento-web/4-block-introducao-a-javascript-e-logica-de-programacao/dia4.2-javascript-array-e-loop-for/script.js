c4b66bb63570de960b18dba915a9b7c88248e51f
Kallyene Evangelista <kallyeneevangelistaadv@gmail.com>
2022-04-10 13:38:05 -0300

    Atividades do dia 4.3

--- 1-fundamentos-do-desenvolvimento-web/4-block-introducao-a-javascript-e-logica-de-programacao/dia4.2-javascript-array-e-loop-for/script.js
+++ 1-fundamentos-do-desenvolvimento-web/4-block-introducao-a-javascript-e-logica-de-programacao/dia4.2-javascript-array-e-loop-for/script.js
@@ -61,13 +61,17 @@ for (name of names) {
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //1.
-for (index = 0; index < numbers.length; index += 1) {
+/* for (index = 0; index < numbers.length; index += 1) {
     console.log(numbers[index])
-}
+} */
 
 
 //2.
+//
+let soma = 0;
+
 for (index = 0; index < numbers.length; index += 1) {
-    let soma = numbers[index] + numbers[index];
-    console.log(soma);
+    soma += numbers[index];
 }
+
+console.log(soma);
