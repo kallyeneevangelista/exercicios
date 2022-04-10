004db27590124f6f751081daa967e0e06d13eaa4
Kallyene Evangelista <kallyeneevangelistaadv@gmail.com>
2022-04-10 14:58:23 -0300

    Atividades pendentes

---
+++ 1-fundamentos-do-desenvolvimento-web/4-block-introducao-a-javascript-e-logica-de-programacao/dia4.3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,20 @@
+/* 1. */
+let numero = 4;
+let numeroDesmembrado = [];
+
+
+/* Algoritmo:
+1. pegar o numero
+2. desmembrar até o número 1
+3. multiplicar um a um
+4. guardar o resultado
+5, consolar
+*/
+
+for (let desmembrar = 0; desmembrar < numero.length, desmembrar -= 1) {
+    console.log(desmembrar)
+}
+/* Não sei como resolver esse problema */
+
+
+/* Bônus */
