cf42919d1b83fbd017e6ed64de4d1854c1df12eb
Kallyene Evangelista <kallyeneevangelistaadv@gmail.com>
2022-04-26 19:33:59 -0300

    Conclusão da atividade do Administrador do Tempo da Trybe

---
+++ 1-fundamentos-do-desenvolvimento-web/5-block-javascript-DOM-eventos-e-web-storage/5.1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,10 @@
+document.getElementById("header-container").style.backgroundColor = "green";
+document.getElementById("footer-container").style.backgroundColor = "green";
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "pink";
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "yellow";
+
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "purple";
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "purple";
+
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "black";
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "black";
