4881eb6470a8506a88efdf9a6849724de816c220
Kallyene Evangelista <kallyeneevangelistaadv@gmail.com>
2022-04-26 19:57:06 -0300

    Criação dos arquivos para atividade complementar

--- 1-fundamentos-do-desenvolvimento-web/5-block-javascript-DOM-eventos-e-web-storage/5.1-javascript-dom-e-seletores/script.js
+++ 1-fundamentos-do-desenvolvimento-web/5-block-javascript-DOM-eventos-e-web-storage/5.1-javascript-dom-e-seletores/script.js
@@ -1,3 +1,5 @@
+/* Exercícios iniciais */
+
 document.getElementById("header-container").style.backgroundColor = "green";
 document.getElementById("footer-container").style.backgroundColor = "green";
 document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "pink";
@@ -8,3 +10,8 @@ document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "pur
 
 document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "black";
 document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "black";
+
+
+
+/* Atividades - Funções de manupulação do DOM */
+
