ac1cc64ddc7b19205b1ba204d8efcab1863db56a
Kallyene Evangelista <kallyeneevangelistaadv@gmail.com>
2022-04-28 11:04:07 -0300

    Refazendo atividade do dia 5.2

--- 1-fundamentos-do-desenvolvimento-web/5-block-javascript-DOM-eventos-e-web-storage/5.2-javascript-trabalhando-com-elementos/script.js
+++ 1-fundamentos-do-desenvolvimento-web/5-block-javascript-DOM-eventos-e-web-storage/5.2-javascript-trabalhando-com-elementos/script.js
@@ -1,30 +1,39 @@
-// 1. Acesse o elemento elementoOndeVoceEsta.
+/* // 1. Acesse o elemento elementoOndeVoceEsta.
 const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
-
 // 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 const pai = elementoOndeVoceEsta.parentElement;
 pai.style.color = 'red';
-
 // 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
 // Você se lembra dos vídeos da aula anterior, como fazer isso?
 const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
 primeiroFilhoDoFilho.innerText = 'primeiroFilhoDoFilho';
-
 // 4. Acesse o primeiroFilho a partir de pai.
 const primeiroFilho = pai.firstElementChild;
-
 // 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
 const primeiroFilho2 = elementoOndeVoceEsta.previousElementSibling;
-
 // 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
 const textElement = elementoOndeVoceEsta.nextSibling;
-
 // 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
 const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
-
 // 8. Agora acesse o terceiroFilho a partir de pai.
 const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
+/* Este é o gabarito. Preciso estudar novamente esse assunto. */ */
 
-/* Este é o gabarito. Preciso estudar novamente esse assunto. */
 
+// 1. Acesse o elemento elementoOndeVoceEsta.
+
+// 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+
+// 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
+// Você se lembra dos vídeos da aula anterior, como fazer isso?
+
+// 4. Acesse o primeiroFilho a partir de pai.
+
+// 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+
+// 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+
+// 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+
+// 8. Agora acesse o terceiroFilho a partir de pai.
 
